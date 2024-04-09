d37294fe5dd5d6f2d29ccb3b82aba92375216984
zef1n <icw20@mail.ru>
2024-04-09 16:26:20 +0500

    Olimp v1.0.3.8.9 - Добавлен домен

--- olympiad/olympiad/settings.py
+++ olympiad/olympiad/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-d!749*23^86bd^dejgv46gw_=^7awa*=v&vnkcecqui&9hpqg$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
