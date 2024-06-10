245f52bd9146a958c7155a50bd06bb30e1944dc5
zef1n <icw20@mail.ru>
2024-06-11 01:01:13 +0500

    fix

--- olympiad/olympiad/asgi.py
+++ olympiad/olympiad/asgi.py
@@ -1,12 +1,13 @@
 import os
-import django
-from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'olympiad.settings')
-django.setup()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "olympiad.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
